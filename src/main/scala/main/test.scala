426eda971f9f3eb6fca827dcbb691e0d28aa9045
Thuy Duong <vuhoang.thduong@gmail.com>
2024-11-27 17:32:46 +0100

    slight change

--- src/main/scala/main/test.scala
+++ src/main/scala/main/test.scala
@@ -26,24 +26,24 @@ object Test {
     val reader1 = ConfigurationParser.getCsvReaderConfigurationFromJson("src/main/resources/Configuration/reader_csv.json")
 
     val testDf1 = reader1.read()
-    testDf1.formatter()
-    testDf1.show()
+    testDf1.formatter().show()
+//    testDf1.show()
 
     // JSON
     println("\n\nJsonReader\n\n")
     val reader2 = ConfigurationParser.getJsonReaderConfigurationFromJson("src/main/resources/Configuration/reader_json.json")
 
     val testDf2 = reader2.read()
-    testDf2.formatter()
-    testDf2.show()
+    testDf2.formatter().show()
+//    testDf2.show()
 
     // XML
     println("\n\nXmlReader\n\n")
     val reader3 = ConfigurationParser.getXmlReaderConfigurationFromJson("src/main/resources/Configuration/reader_xml.json")
 
     val testDf3 = reader3.read()
-    testDf3.formatter()
-    testDf3.show()
+    testDf3.formatter().show()
+//    testDf3.show()
 
   }
 }
