6d7aca0bd3c4e9cfe83ace89894d56c7a9ae7474
InesH222 <ineshummel75116@gmail.com>
2024-12-06 15:15:14 +0100

    Update ServiceVente.scala

--- src/main/scala/traitement/ServiceVente.scala
+++ src/main/scala/traitement/ServiceVente.scala
@@ -39,7 +39,7 @@ from pyspark.sql.functions import regexp_extract
 
 df_with_columns = df.select(
     # Extraire la date à l'aide d'une expression régulière
-    regexp_extract(col("json_column"), r'"Date_End_contrat":\s?"(\d{4}-\d{2}-\d{2})"', 1).alias("Date_End_contrat"),
+    regexp_extract(col("json_column"), r'"Date_End_contrat":\s?data.json', 1).alias("Date_End_contrat"),
     # Extraire la ville à l'aide d'une expression régulière
     regexp_extract(col("json_column"), r'"Ville":\s?"([^"]+)"', 1).alias("Ville")
 )
