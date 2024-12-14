1e5f839422e8863e14a587a122bac8e0c9ad9a70
Thuy Duong <vuhoang.thduong@gmail.com>
2024-12-14 20:52:27 +0100

    update

--- src/main/scala/traitement/ServiceVente.scala
+++ src/main/scala/traitement/ServiceVente.scala
@@ -14,6 +14,10 @@ object ServiceVente {
         .withColumn("TVA", split(col("HTT_TVA"), "\\|")(1))
     }
 
+    def calculTTC() : DataFrame = {
+      
+    }
+
 //    def calculTTC () : DataFrame = {
 //      /*..........................coder ici...............................*/
 //    }
@@ -35,13 +39,3 @@ object ServiceVente {
   }
 
 }
-from pyspark.sql.functions import regexp_extract
-
-df_with_columns = df.select(
-    # Extraire la date à l'aide d'une expression régulière
-    regexp_extract(col("json_column"), r'"Date_End_contrat":\s?'data.json', 1).alias("Date_End_contrat"),
-    # Extraire la ville à l'aide d'une expression régulière
-    regexp_extract(col("json_column"), r'"Ville":\s?'data.json', 1).alias("Ville")
-)
-
-df_with_columns.show()
